73fab8214ea052cf0f73958f11270ff9f47f025a
Gwonsoo Che <gwonsoo.che@gmail.com>
2023-06-21 14:57:11 +0900

    Serve the frontend using static files
    We will for now serve the frontend using express.static, rather than building
    the frontend as an independent application.

--- chat-server/server.js
+++ chat-server/server.js
@@ -1,11 +1,12 @@
+const path = require('path'); // core module of Node.js
 const express = require('express');
+
 const app = express();
+// Serve the frontend using static files, rather than an independent
+// frontend application.
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3002;
-
-app.get('/', (req, res) => {
-  res.send('Hello')
-});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
