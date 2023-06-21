661499e8881d6c6916e4fd5b57af42465cd6c616
Gwonsoo Che <gwonsoo.che@gmail.com>
2023-06-21 15:08:29 +0900

    Create socket connection between client and server

--- chat-server/server.js
+++ chat-server/server.js
@@ -1,13 +1,22 @@
 const path = require('path'); // core module of Node.js
 const express = require('express');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
 
 const app = express();
 // Serve the frontend using static files, rather than an independent
 // frontend application.
 app.use(express.static(path.join(__dirname, 'public')));
 
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options, if any */ });
+
+io.on('connection', (socket) => {
+  console.log(`Created socket connection with socket id ${socket.id}`);
+});
+
 const PORT = process.env.PORT || 3002;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+httpServer.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
