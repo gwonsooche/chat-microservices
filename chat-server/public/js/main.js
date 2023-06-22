fcad558ecb52a9c5fe126f4ec07813bcfdeedc1c
Gwonsoo Che <gwonsoo.che@gmail.com>
2023-06-23 07:30:37 +0900

    Emit the very first message when a client joins
    When a client joins a channel, the following two kinds of messages are sent:
    * (to the client) "Welcome to [channel name]!"
    * (to the other clients in the channel) "[client] has joined the channel."

--- chat-server/public/js/main.js
+++ chat-server/public/js/main.js
@@ -1,2 +1,25 @@
 // Connect to server.
-const socket = io()
+const socket = io();
+
+const chatMessages = document.querySelector('.chat-messages');
+
+const {username, channel} = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
+
+socket.timeout(2000).emit('join-channel', {username, channel});
+
+socket.on('message', (message) => {
+  console.log(message);
+  writeMessage(message);
+
+  // Scroll down automatically.
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+function writeMessage(message) {
+  const div = document.createElement('div');
+  div.classList.add('message');
+  div.innerHTML = `<p class='text'>${message}</p>`;
+  document.querySelector('.chat-messages').appendChild(div);
+}
--- chat-server/server.js
+++ chat-server/server.js
@@ -13,6 +13,21 @@ const io = new Server(httpServer, { /* options, if any */ });
 
 io.on('connection', (socket) => {
   console.log(`Created socket connection with socket id ${socket.id}`);
+
+  socket.on('join-channel', ({username, channel}) => {
+    socket.join(channel);
+
+    // Emit welcome message to the single client that's newly joining the
+    // channel.
+    socket.emit('message', `Welcome to ${channel}!`);
+
+    // Emit to all the other clients in the channel, except the client that's
+    // newly joining.
+    socket
+        .broadcast
+        .to(channel)
+        .emit('message', `${username} has joined the channel.`);
+  });
 });
 
 const PORT = process.env.PORT || 3002;
