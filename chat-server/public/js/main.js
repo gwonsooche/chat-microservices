fd6798b1ad4c9e2acc7f6256ddab31faa5b0355b
Gwonsoo Che <gwonsoo.che@gmail.com>
2023-06-23 15:02:15 +0900

    Enable the client to send messages to the server
    - Only the client side is implemented.
    - TODO: broadcasting from the server.

--- chat-server/public/js/main.js
+++ chat-server/public/js/main.js
@@ -2,6 +2,7 @@
 const socket = io();
 
 const chatMessages = document.querySelector('.chat-messages');
+const chatForm = document.getElementById('chat-form');
 
 const {username, channel} = Qs.parse(location.search, {
   ignoreQueryPrefix: true,
@@ -15,6 +16,19 @@ socket.on('message', (message) => {
 
   // Scroll down automatically.
   chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+chatForm.addEventListener('submit', (event) => {
+  // The default behavior is to submit the form data to the current URL
+  // (main.js) with the GET method. We don't want to do that.
+  event.preventDefault();
+
+  // Emit the submitted text to the server.
+  socket.emit('chat-message', event.target.querySelector('#submit-text').value);
+
+  // Clear the input text.
+  event.target.querySelector('#submit-text').value = '';
+  event.target.querySelector('#submit-text').focus();
 });
 
 function writeMessage(message) {
