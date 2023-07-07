acce48d133ea454d73f6dfc4ecdc1404be77973b
Gwonsoo Che <gwonsoo.che@gmail.com>
2023-07-07 20:24:20 +0900

    Export the User

--- chat-server/user.js
+++ chat-server/user.js
@@ -1,6 +1,13 @@
-/** User constructor */
+/**
+ * User of the chat application.
+ * @constructor
+*/
 function User(socketId, username, channel) {
   this.id = socketId;
   this.username = username;
   this.channel = channel;
 }
+
+module.exports = {
+  User,
+};
